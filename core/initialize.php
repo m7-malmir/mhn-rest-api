4aa01ae1e55e6a7c6babac51bf29b243805eccd9
unknown <administrator@M-MALMIR-A.MHN.local>
2024-03-18 09:30:50 +0330

    read single from api was complete

--- core/initialize.php
+++ core/initialize.php
@@ -2,18 +2,21 @@
 
 
 
+
+
+
 defined('DS') ? null : define('DS',DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define('SITE_ROOT',DS.'wamp64'.DS.'www'.DS.'phprest');
+defined('SITE_ROOT') ? null : define('SITE_ROOT','xampp'.DS.'localhost'.DS.'REST-API');
 defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
 defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'core');
 
 
 
 //load the config file first
-require_once(INC_PATH.DS."config.php");
+require_once("../includes/config.php");
 
 //core classes
-require_once(INC_PATH.DS."post.php");
+require_once("post.php");
 
 
 
--- core/post.php
+++ core/post.php
@@ -21,13 +21,53 @@ class Post{
     //getting post from our database
     public function read(){
         //create query
-        $query='SELECT c.name as category_name, p.id, p.category_id,p.title,p.body,p.author,p.created_at FROM '.$this->table.' p LEFT JOIN categoties c ON p.categories_id = c.id ORDERED BY p.created DESC';
+        $query='SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at FROM posts p LEFT JOIN categories c ON p.category_id =c.id ORDER BY p.created_at DESC';
         
         //prepare statement
         $stmt =$this->conn->prepare($query);
         //execute query
         $stmt->execute();
         return $stmt;
+    }
+     public function read_single(){
+        $query='SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at FROM posts p LEFT JOIN categories c ON p.category_id =c.id WHERE p.id =? LIMIT 1';
+        
+        //prepare statement
+        $stmt =$this->conn->prepare($query);
+        $stmt->bindParam(1,$this->id);
+        $stmt->execute();
+        $row=$stmt->fetch(PDO::FETCH_ASSOC);
+        $this->title=$row['title'];
+        $this->body=$row['body'];
+        $this->author=$row['author'];
+        $this->category_id=$row['category_id'];
+        $this->category_name=$row['category_name'];
+     }
+     public function create(){
+        //create query
+        $query='INSERT INTO '. $this->table.' SET title= :title , body = :body,author= :author,category_id= :category_id';
+        //prapare statement
+        $stmt=$this->conn->prepare($query);
+
+        //clean data
+        $this->title=htmlspecialchars(strip_tags($this->title));
+        $this->body=htmlspecialchars(strip_tags($this->body));
+        $this->author=htmlspecialchars(strip_tags($this->author));
+        $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+        
+        //binding parameters
+        $stmt->bindParam(':title',$this->title);
+        $stmt->bindParam(':body',$this->body);
+        $stmt->bindParam(':author',$this->author);
+        $stmt->bindParam(':category_id',$this->category_id);
+
+        //ecexute query
+        if($stmt->execute()){
+            return true;
+        }
+        printf("Error %s. \n",$stmt->error);
+        return false;
+    
     }
 
 }
